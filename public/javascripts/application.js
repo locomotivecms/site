4e02edd01573548d933de6b6a0314a9ffbd4da1c
dinedine <didier.lafforgue@gmail.com>
2011-01-21 10:53:45 +0100

    the features page is done

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
   if (typeof($.fn.fancybox) != 'undefined')
-    $("#features li div.image a").fancybox({ 'overlayOpacity': 0.7, 'overlayColor': '#222' });
-
-  $('#accordion').accordion({
-    collapsible: true,
-    active: false,
-    autoHeight: false
-  });
+    $(".feature div.image a").fancybox({ 'overlayOpacity': 0.7, 'overlayColor': '#222' });
 });
