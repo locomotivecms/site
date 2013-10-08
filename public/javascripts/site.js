39987a2aa29807f5fcc902445258af4ff9859e25
Adam Rice <development@hashnotadam.com>
2013-10-09 10:22:55 +1100

    Fix javascript bug causing mobile nav not to work

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,6 +1,6 @@
 $(function() {
   // mobile
   $('#nav-toggle').on('click',function(e){
-    $(this).$('#main-nav').toggleClass('expanded');
+    $(this).add('#main-nav').toggleClass('expanded');
   });
 });
